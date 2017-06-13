a10129ac02df73a970d49fdfd8626b3b72196d8b
Dheeraj Kumar Barnwal <dheerajraaj@Dheerajs-MacBook-Air.local>
2017-06-13 22:50:26 +0530

    adding all sets of 3 numbers whose sum equals to 0

---
+++ ThreeSum.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+/**
+ * Created by dheerajraaj on 13/06/17.
+ */
+public class ThreeSum {
+
+    public void findTriplet(int arr[]){
+        Boolean found = false;
+        Arrays.sort(arr);
+        int length = arr.length;
+        for(int i=0; i<length-2; i++){
+            int j=i+1,k=length-1;
+            while(j < k){
+                if(arr[i]+arr[j]+arr[k] < 0){
+                    j++;
+                }else if(arr[i]+arr[j]+arr[k] > 0){
+                    k--;
+                }else{
+                    System.out.println("Triplet = " + arr[i] + " " + arr[j] + " " + arr[k]);
+                    j++;
+                    k--;
+                    found = true;
+                }
+            }
+
+
+        }
+        if(found == false){
+            System.out.println("No Triplet found");
+        }
+    }
+    public static void main(String args[]){
+        ThreeSum ts = new ThreeSum();
+        int arr[] = {0, -1, 2, -3, 1};
+        ts.findTriplet(arr);
+    }
+}
