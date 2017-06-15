20da6a4e5d9d3c201aa6c61d3b3e11d8c8ff0408
Dheeraj Kumar Barnwal <dheerajraaj@Dheerajs-MacBook-Air.local>
2017-06-15 20:43:48 +0530

    find triplet whose sumequal given sum

---
+++ TripletSum.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+/**
+ * Created by Dheeraj Kumar Barnwal on 13/06/17.
+ */
+public class TripletSum {
+
+    public void findTriplet(int arr[], int sum){
+        Boolean found = false;
+        Arrays.sort(arr);
+        int length = arr.length;
+        for(int i=0; i<length-2; i++){
+            int j=i+1,k=length-1;
+            while(j < k){
+                if(arr[i]+arr[j]+arr[k] < sum){
+                    j++;
+                }else if(arr[i]+arr[j]+arr[k] > sum){
+                    k--;
+                }else{
+                    System.out.println("Triplet = " + arr[i] + " " + arr[j] + " " + arr[k]);
+                    j++;
+                    k--;
+                    found = true;
+                }
+            }
+
+
+        }
+        if(found == false){
+            System.out.println("No Triplet found");
+        }
+    }
+    public static void main(String args[]){
+        TripletSum ts = new TripletSum();
+        int arr[] = {0, -1, 2, -3, 1, 3};
+        int sum = 3;
+        ts.findTriplet(arr, sum);
+    }
+}
